8cb0fd4676b210b3ccae4e9e1c1c532a83efa995
guptaananya-lab <gupta.ananya.cse@gmail.com>
2026-03-11 00:05:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+const URL = "https://teachablemachine.withgoogle.com/models/pFSaUVG0q/";
+
+let model;
+
+async function loadModel(){
+model = await tmImage.load(URL + "model.json", URL + "metadata.json");
+}
+
+loadModel();
+
+async function detectWaste(){
+
+const input = document.getElementById("imageUpload");
+
+if(input.files.length === 0){
+document.getElementById("result").innerHTML="Please upload image";
+return;
+}
+
+const file = input.files[0];
+
+const img = new Image();
+img.src = window.URL.createObjectURL(file);
+
+img.onload = async function(){
+
+const prediction = await model.predict(img);
+
+let highest = prediction[0];
+
+for(let i=1;i<prediction.length;i++){
+if(prediction[i].probability > highest.probability){
+highest = prediction[i];
+}
+}
+
+document.getElementById("result").innerHTML =
+"Detected Waste: " + highest.className;
+
+};
+
+}
