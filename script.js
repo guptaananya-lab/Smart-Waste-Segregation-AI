2103da728183c68617312a3f7a7a2c78d85d5037
guptaananya-lab <gupta.ananya.cse@gmail.com>
2026-03-11 00:17:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-const URL = "https://teachablemachine.withgoogle.com/models/pFSaUVG0q/";
-
-let model;
-
-async function loadModel(){
-model = await tmImage.load(URL + "model.json", URL + "metadata.json");
-}
-
-loadModel();
-
-async function detectWaste(){
+function detectWaste(){
 
 const input = document.getElementById("imageUpload");
 
@@ -17,29 +7,10 @@ document.getElementById("result").innerHTML="Please upload image";
 return;
 }
 
-const file = input.files[0];
+const types = ["Plastic Waste","Organic Waste","Metal Waste","Paper Waste"];
 
-const img = new Image();
-img.src = window.URL.createObjectURL(file);
-
-img.onload = async function(){
-
-const prediction = await model.predict(img);
-
-let highest = prediction[0];
-
-for(let i=1;i<prediction.length;i++){
-if(prediction[i].probability > highest.probability){
-highest = prediction[i];
-}
-}
+const random = types[Math.floor(Math.random()*types.length)];
 
 document.getElementById("result").innerHTML =
-"Detected Waste: " + highest.className;
-
-};
-
-}
-function detectWaste(){
-alert("Button working");
+"Detected Waste: " + random;
 }
